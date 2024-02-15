9727ff82d6037d3854b67148211bac718b36c9bd
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-02-15 10:37:58 +0100

    Add colors when to mouse hover
    Add button to change grit size by user

--- js-file.js
+++ js-file.js
@@ -2,27 +2,47 @@ const container = document.querySelector(".container");
 
 
 
-let squaresSize = prompt("Please enter grid size", "1-100");
-if (squaresSize < 1 || squaresSize > 100){
-  document.querySelector(".text").textContent = "Input incorrect. Setting default 16 grid";
-  squaresSize = 16;
+createGrid()
 
-}
+const btn = document.querySelector(".btn");
 
 
-for (let i = 0; i < squaresSize ** 2; i++){
-  const divChild = document.createElement("div");
-  container.appendChild(divChild);
-  divChild.classList.add("square");
+
+btn.addEventListener("click", () => {
+  squaresSize = parseInt(prompt("Please enter grid size", "1-100"));
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
   }
+  createGrid(squaresSize)
 
-let squareWidthHeight = Math.round((600 / squaresSize) * 100) / 100;
-const squares = document.querySelectorAll(".square")
-squares.forEach((square) => {
-  square.style.width = squareWidthHeight + "px";
-  square.style.height = squareWidthHeight + "px";
-  square.addEventListener("mouseenter", () => {
-    square.style.backgroundColor = "red"
-  });
 });
 
+
+function createGrid (squaresSize = 16){
+  if (squaresSize < 1 || squaresSize > 100){
+    alert("Input incorrect. Setting default 16 grid")
+    // document.querySelector(".text").textContent = "Input incorrect. Setting default 16 grid";
+    squaresSize = 16;
+  }
+
+  for (let i = 0; i < squaresSize ** 2; i++){
+    const divChild = document.createElement("div");
+    container.appendChild(divChild);
+    divChild.classList.add("square");
+  }
+
+  let squareWidthHeight = Math.round((600 / squaresSize) * 100) / 100;
+  const squares = document.querySelectorAll(".square")
+  squares.forEach((square) => {
+    square.style.width = squareWidthHeight + "px";
+    square.style.height = squareWidthHeight + "px";
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = colorChange();
+    });
+  });
+}
+
+function colorChange() {
+  randomHexNumber = Math.floor(Math.random()*16777215).toString(16);
+  return "#" + randomHexNumber;
+}
